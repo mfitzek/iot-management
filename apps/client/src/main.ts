d3a7b3fef70ba9ba7592d5334d18563b1f88d16d
mfitzek <m4rfitzek@gmail.com>
2022-09-10 17:56:22 +0200

    init vue

---
+++ apps/client/src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/cs'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+
+import 'quasar/dist/quasar.css'
+
+const app = createApp(App);
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+})
+app.mount('#app');
