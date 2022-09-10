737676be107808fe3c871cdabe37717d22bfc4e3
mfitzek <m4rfitzek@gmail.com>
2022-09-10 21:11:12 +0200

    shared lib

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({
     template: { transformAssetUrls }
   }),
-  quasar()
+  quasar(),
+  tsconfigPaths({
+    projects: ["tsconfig.json", "../api/" ]
+  }),
 ],
 });
---
+++ libs/user/src/lib/user.ts
@@ -0,0 +1,7 @@
+export function user(): string {
+  return 'user';
+}
+
+
+
+export const Users = "Hello User Lib."
