a0c2f8e5a2d48ac874f016eb233fd6273695c453
mfitzek <m4rfitzek@gmail.com>
2022-09-12 09:03:39 +0200

    Device

---
+++ apps/client/src/layouts/DeviceLayout.vue
@@ -0,0 +1,3 @@
+<template>
+    <component></component>
+</template>
--- apps/client/src/pages/device/DeviceList.vue
+++ apps/client/src/pages/device/DeviceList.vue
@@ -2,14 +2,17 @@
   <div class="q-pa-md">
     <div class="row items-center justify-between">
       <div class="col-auto">
-        <h1>Device list</h1>
+        <h3>Device list</h3>
       </div>
       <div class="col-auto">
-        <q-btn color="green" icon-right="add">Add device</q-btn>
+        <q-btn color="green" icon-right ="add">Add device</q-btn>
       </div>
     </div>
     <div>
-    <q-table :columns="columns" :rows="rows">
+      
+    <component is="h1">Hello</component>
+
+    <q-table :columns="columns" :rows="rows" @row-click="clickDevice">
       <template #body-cell-status="{ row }">
         <q-td class="text-right">
           <q-badge rounded :color="row_status(row.status)"></q-badge>
@@ -30,13 +33,36 @@ const columns: QTableColumn[] = [
   { name: 'status', label: 'Status', field: 'status' },
 ];
 
-const rows = [
+interface IDevice {
+  id: number;
+  name: string;
+  type: string;
+  last_data: string;
+  status: string;
+}
+
+const rows: IDevice[] = [
   {
+    id: 100,
     name: 'Test',
     type: 'X752-AB',
-    last_data: '11.09.2022 13:52',
+    last_data: '12.09.2022 13:52',
     status: 'online',
   },
+  {
+    id: 101,
+    name: 'Test2',
+    type: 'S3X-BD',
+    last_data: '12.09.2022 10:52',
+    status: 'warning',
+  },
+  {
+    id: 102,
+    name: 'Test3',
+    type: 'S3X-CMD',
+    last_data: '01.08.2022 07:52',
+    status: 'error',
+  }
 ];
 
 const row_status = (status: string) => {
@@ -47,6 +73,11 @@ const row_status = (status: string) => {
   };
   return colors[status] ?? 'red';
 };
+
+async function clickDevice({}, row: IDevice){
+  console.log('clicked device', row.id);
+}
+
 </script>
 
 <style scoped></style>
