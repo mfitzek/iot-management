f76e4878513325f05296fac3f4d401d6f5b2b3bb
mfitzek <m4rfitzek@gmail.com>
2022-09-19 22:27:11 +0200

    password hashing

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,12 +1,11 @@
-import { Prisma, User } from '@prisma/client';
-import { UserService } from '../user/user.service';
+import { AuthService } from './auth.service';
 import { ILoginPost, IRegisterPost, IUser } from '@iot/user';
 import { Body, Controller, Post } from '@nestjs/common';
 import CreatingUserError from 'libs/user/src/errors/CreatingUser';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private users: UserService) {}
+  constructor(private auth: AuthService) {}
 
   @Post('login')
   async login(@Body() credentials: ILoginPost) {
@@ -15,20 +14,21 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() credentials: IRegisterPost) {
-    const result = await this.users.createUser({
-      username: credentials.username,
-      email: credentials.email,
-      password: credentials.password,
-    });
+    // const result = await this.users.createUser({
+    //   username: credentials.username,
+    //   email: credentials.email,
+    //   password: credentials.password,
+    // });
 
-    const user = result as User;
-    if (user.username) {
-      return { username: user.username };
-    } else {
-      console.log('test');
-      return {
-        errors: (result as CreatingUserError[]).map((err) => err.message),
-      };
-    }
+    // const user = result as User;
+    // if (user.username) {
+    //   return { username: user.username };
+    // } else {
+    //   console.log('test');
+    //   return {
+    //     errors: (result as CreatingUserError[]).map((err) => err.message),
+    //   };
+    // }
+    return this.auth.register(credentials);
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { IRegisterPost } from '@iot/user';
+
+import { randomBytes, pbkdf2 } from 'crypto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async register(data: IRegisterPost) {
+    return await this.createPasswordHash(data.password);
+  }
+
+  private createPasswordHash(
+    password: string
+  ): Promise<{ hash: string; salt: string }> {
+    return new Promise((resolve, reject) => {
+      const salt = randomBytes(16).toString('hex');
+      pbkdf2(password, salt, 1000, 64, 'sha512', (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        const hash = result.toString('hex');
+        resolve({ hash, salt });
+      });
+    });
+  }
+
+  private comparePasswords(password, hash, salt): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      pbkdf2(password, salt, 1000, 64, 'sha512', (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        const hashed_password = result.toString('hex');
+
+        resolve(hashed_password === hash);
+      });
+    });
+  }
+}
