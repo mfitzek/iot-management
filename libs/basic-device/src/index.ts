5f9602ce3aaca3b5aae7729c49eac2d628ec4b94
mfitzek <m4rfitzek@gmail.com>
2022-10-04 22:57:07 +0200

    async component

--- libs/basic-device/src/index.ts
+++ libs/basic-device/src/index.ts
@@ -1,6 +1,6 @@
-import component from './ui/BasicDevice.vue';
+import { defineAsyncComponent } from 'vue';
 
 export default class BasicDevice {
   public readonly type = 'basic-device';
-  public component = component;
+  public component = defineAsyncComponent(() => import('./ui/BasicDevice.vue'));
 }
