5136fd4edc386f0bf2135d563840914ab87caae2
Martin Fitzek <m4rfitzek@gmail.com>
2022-11-07 15:24:39 +0100

    maybe done

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,18 +1,21 @@
 const { VueLoaderPlugin } = require('vue-loader');
 
-console.log('WEBPACK CONFIG LOAD !!! 🚀');
+const { merge } = require('webpack-merge');
 
-module.exports = function (options) {
-  return {
-    ...options,
+module.exports = (config, context) => {
+  return merge(config, {
     module: {
       rules: [
         {
           test: /\.vue$/,
           loader: 'vue-loader',
         },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+        },
       ],
     },
     plugins: [new VueLoaderPlugin()],
-  };
+  });
 };
--- libs/basic-device/src/index.ts
+++ libs/basic-device/src/index.ts
@@ -3,14 +3,12 @@ import { Device, IDevice, IDeviceData, IDeviceService } from '@iot/device';
 import { defineAsyncComponent } from 'vue';
 
 export class BasicDevice implements ICustomDevice {
-
   factory(data: IDeviceData, device_service: IDeviceService): IDevice {
-      return new Device(device_service, data);
+    return new Device(device_service, data);
   }
 
   type = 'basic-device';
   component = defineAsyncComponent(() => import('./ui/BasicDevice.vue'));
-
-};
+}
 
 export default new BasicDevice();
