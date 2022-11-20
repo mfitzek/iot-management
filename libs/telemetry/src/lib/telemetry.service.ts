942aa9ec4a3998d60dfa12644230a99962286beb
Martin Fitzek <m4rfitzek@gmail.com>
2022-11-20 14:06:41 +0100

    inserting data via mqtt

--- libs/telemetry/src/lib/telemetry.service.ts
+++ libs/telemetry/src/lib/telemetry.service.ts
@@ -1,6 +1,8 @@
 import { ITelemetry } from './../interface/ITelemetry';
 import { Injectable } from '@nestjs/common';
 
+import {PrismaClient} from "@prisma/client"
+
 import {
   ISearchTelemetry,
   ITelemetryService,
@@ -9,11 +11,37 @@ import {
 @Injectable()
 export class TelemetryService implements ITelemetryService {
   private telemetry: ITelemetry[] = [];
+  private prisma = new PrismaClient();
 
   async getTelemetry(filter: ISearchTelemetry): Promise<ITelemetry[]> {
-    return this.telemetry;
+    const data = await this.prisma.telemetry.findMany({
+      where: {
+        id: {
+          in: filter.attribute_ids
+        }
+      }
+    });
+
+    return data.map(t=>{
+      return {
+        attribute_id: t.attributeId,
+        value: t.value,
+        createdAt: t.createdAt
+      }
+    })
+
   }
   saveTelemetry(telemetry: ITelemetry) {
     this.telemetry.push(telemetry);
+    console.log(telemetry);
+    this.prisma.telemetry.create({data: {
+      attributeId: telemetry.attribute_id,
+      value: telemetry.value,
+      createdAt: telemetry.createdAt
+    }}).then(t=>{
+      console.log("inserted", t);
+    }).catch(e=>{
+      console.log(e);
+    })
   }
 }
