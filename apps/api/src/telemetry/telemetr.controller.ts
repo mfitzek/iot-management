015f44d8f7bc3eb1ae2a9345a6f0dd1ab18244a9
Martin Fitzek <m4rfitzek@gmail.com>
2022-11-25 10:54:29 +0100

    parse search attributes

--- apps/api/src/telemetry/telemetr.controller.ts
+++ apps/api/src/telemetry/telemetr.controller.ts
@@ -1,3 +1,4 @@
+import { ISearchTelemetry } from '@iot/telemetry';
 import { Controller, Get, UseGuards, Req, Query } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
@@ -5,7 +6,35 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 @Controller('telemetry')
 export class TelemetryController {
   @Get()
-  async getTelemetry(@Req() req, @Query('attr') attributesIds: string[]) {
-    return attributesIds;
+  async getTelemetry(
+    @Req() req,
+    @Query('attr') attributesIds: string[],
+    @Query('start') dateFrom: string,
+    @Query('end') dateTo: string
+  ) {
+    const dateFromNum = Number(dateFrom);
+    const dateToNum = Number(dateTo);
+
+    let searchFrom: Date | undefined = new Date(dateFromNum);
+    let searchTo: Date | undefined = new Date(dateToNum);
+
+    if (isNaN(searchFrom.getDate())) {
+      searchFrom = undefined;
+    }
+    if (isNaN(searchTo.getDate())) {
+      searchTo = undefined;
+    }
+
+    const search: ISearchTelemetry = {
+      attribute_ids: attributesIds,
+      date_from: searchFrom,
+      date_to: searchTo,
+    };
+
+    return {
+      attributesIds,
+      searchFrom,
+      searchTo,
+    };
   }
 }
