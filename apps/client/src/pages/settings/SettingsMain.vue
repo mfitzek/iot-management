2adc95deff82f3e7ee9d3f0e80d7631d67fb84dc
Martin Fitzek <m4rfitzek@gmail.com>
2022-11-27 15:09:18 +0100

    settings

--- apps/client/src/pages/settings/SettingsMain.vue
+++ apps/client/src/pages/settings/SettingsMain.vue
@@ -6,16 +6,90 @@
       <DBState></DBState>
     </section>
 
-    <section>
-      FILE
-      <q-btn color="primary" icon="s_file_upload" label="Upload Backup" />
-      <q-btn color="primary" icon="s_file_download" label="Create Backup" />
+    <section class="q-mt-md">
+      <p class="text-h5">Backup Database</p>
+      <q-btn
+        color="primary"
+        icon="cloud_download"
+        label="Backup"
+        @click="backup"
+      />
+    </section>
+
+    <section class="q-mt-md">
+      <p class="text-h5">Restore Database</p>
+      <div class="row items-center q-gutter-md">
+        <div class="col-4">
+          <q-file
+            filled
+            bottom-slots
+            v-model="file"
+            label="Label"
+            counter
+            accept=".zip"
+          >
+            <template v-slot:prepend>
+              <q-icon name="cloud_upload" @click.stop />
+            </template>
+            <template v-slot:append>
+              <q-icon
+                name="close"
+                @click.stop="file = null"
+                class="cursor-pointer"
+              />
+            </template>
+          </q-file>
+        </div>
+        <div class="col-auto">
+          <q-btn
+            color="primary"
+            icon="cloud_upload"
+            label="Restore"
+            @click="restore"
+          />
+        </div>
+      </div>
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
 import DBState from '../../components/settings/DBState.vue';
+import { ref } from 'vue';
+import api from '@iot/services/http';
+
+const file = ref(null);
+
+async function restore() {
+  if (file.value) {
+    let form = new FormData();
+    form.append('backup', file.value);
+    try {
+      await api.post('/settings/restore', form);
+      // this.$store.dispatch("user_settings/fetch");
+      // this.$store.commit("app/Push", { message: "Databáze úspěšně obnovena", type: "success" });
+    } catch (error) {
+      // this.$store.commit("app/Push", {
+      //     message: "Chyba při obnovování dat.",
+      //     type: "error",
+      // });
+      console.log(error);
+    }
+  }
+}
+async function backup() {
+  const res = await api.post('/settings/backup', null, {
+    responseType: 'blob',
+  });
+  const filename = res.headers['content-disposition'].split('=')[1];
+  const url = window.URL.createObjectURL(new Blob([res.data]));
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 </script>
 
 <style scoped></style>
