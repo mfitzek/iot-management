1d374c9675b72a7a2a5b6aa336dd07e859aa2766
Martin Fitzek <m4rfitzek@gmail.com>
2022-12-05 15:43:05 +0100

    wip

--- libs/telemetry/src/lib/TelemetryCache.ts
+++ libs/telemetry/src/lib/TelemetryCache.ts
@@ -6,26 +6,10 @@ export class TelemetryCache {
   private prisma = new PrismaClient();
 
   private primaryActive = true;
-  private cacheCountLimit = 1000;
-  private cacheTimeLimitMS = 5 * 60 * 1000;
+  private cacheRecordsLimit = 1000;
+  private cacheTimeLimitMS = 5 * 60 * 1000; // TODO implement cache timeout to write DB
   private primaryCache: ITelemetry[] = [];
   private secondaryCache: ITelemetry[] = [];
-
-  private async writeCacheToDatabase(cache: ITelemetry[]) {
-    const transactions = cache.map((row) => {
-      return this.prisma.telemetry.create({
-        data: {
-          attributeId: row.attribute_id,
-          createdAt: row.createdAt ?? new Date(),
-          value: row.value,
-        },
-      });
-    });
-
-    await this.prisma.$transaction(transactions);
-
-    return;
-  }
 
   public getTelemetry(filter: ISearchTelemetry) {
     const currentCache: ITelemetry[] = []; // TODO: get current cache
@@ -43,11 +27,44 @@ export class TelemetryCache {
     });
   }
 
-  public async saveTelemetry(telemetry: ITelemetry) {
+  public saveTelemetry(telemetry: ITelemetry) {
     if (this.primaryActive) {
-      this.primaryCache.push(telemetry);
+      this.saveToPrimaryCache(telemetry);
     } else {
-      this.secondaryCache.push(telemetry);
+      this.saveToSecondaryCache(telemetry);
     }
+  }
+
+  private async saveToPrimaryCache(telemetry: ITelemetry) {
+    this.primaryCache.push(telemetry);
+    if (this.primaryCache.length >= this.cacheRecordsLimit) {
+      this.primaryActive = false;
+      await this.writeCacheToDatabase(this.primaryCache);
+      this.primaryCache = [];
+    }
+  }
+  private async saveToSecondaryCache(telemetry: ITelemetry) {
+    this.secondaryCache.push(telemetry);
+    if (this.secondaryCache.length >= this.cacheRecordsLimit) {
+      this.primaryActive = true;
+      await this.writeCacheToDatabase(this.secondaryCache);
+      this.secondaryCache = [];
+    }
+  }
+
+  private async writeCacheToDatabase(cache: ITelemetry[]) {
+    const transactions = cache.map((row) => {
+      return this.prisma.telemetry.create({
+        data: {
+          attributeId: row.attribute_id,
+          createdAt: row.createdAt ?? new Date(),
+          value: row.value,
+        },
+      });
+    });
+
+    await this.prisma.$transaction(transactions);
+
+    return;
   }
 }
