81e5fa02b2e99757d4bbb93390864e511945ee7c
Martin Fitzek <m4rfitzek@gmail.com>
2022-12-11 22:52:04 +0100

    storage wip

--- libs/configuration/src/index.ts
+++ libs/configuration/src/index.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Settings } from './dataObjects';
+import { JsonSettingsStorage } from './settings-storage/jsonSettingsStorage';
+import { SettingsStorage } from './settings-storage/settingsStorage';
 
 @Injectable()
 export class Configuration {
   private settings?: Settings;
+  public storage: SettingsStorage;
 
-  constructor() {}
+  constructor() {
+    this.storage = new JsonSettingsStorage();
+  }
+
+  public async getSettings(): Promise<Settings> {
+    if (!this.settings) {
+      this.settings = await this.storage.getSettings();
+    }
+    return { ...this.settings };
+  }
+
+  public async saveSettings(settings: Settings) {
+    this.settings = settings;
+    await this.storage.saveSettings(settings);
+  }
 }
---
+++ libs/configuration/src/settings-storage/jsonSettingsStorage.ts
@@ -0,0 +1,26 @@
+import { Settings } from "../dataObjects";
+import { SettingsStorage } from "./settingsStorage";
+
+import {writeFile, readFile, } from "fs/promises";
+
+
+export class JsonSettingsStorage implements SettingsStorage {
+
+    filePath = "../../../../settings.json";
+
+    async saveSettings(settings: Settings): Promise<boolean> {
+        const jsonString = JSON.stringify(settings);
+        try {
+            await writeFile(this.filePath, jsonString);
+        } catch (error) {
+            return false;
+        }
+        return true;
+    }
+    async getSettings(): Promise<Settings> {
+        const fileBuffer = await readFile(this.filePath);
+        const settings = JSON.parse(fileBuffer.toString());
+        return settings as Settings;
+    }
+    
+}
