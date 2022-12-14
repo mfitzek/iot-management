2aeab814a784d6a2de46e069e089467ee91dc1a3
Martin Fitzek <m4rfitzek@gmail.com>
2022-12-14 15:38:26 +0100

    utlity lib

--- libs/telemetry/src/lib/MainTelemetryCollector.ts
+++ libs/telemetry/src/lib/MainTelemetryCollector.ts
@@ -4,14 +4,13 @@ import { ITelemetry } from '../interface/ITelemetry';
 import { TelemetryCollector } from '../interface/TelemetryCollector';
 import { CacheTelemetryCollector } from './CacheTelemetryCollector';
 import { DatabaseTelemetryCollector } from './DatabaseTelemetryCollector';
-
+import { ConfiguratioProvider } from '@iot/configuration';
 @Injectable()
 export class MainTelemetryCollector implements TelemetryCollector {
-
   private cacheCollector: CacheTelemetryCollector;
   private databaseCollector: DatabaseTelemetryCollector;
 
-  constructor(){
+  constructor(private configuration: ConfiguratioProvider) {
     this.cacheCollector = new CacheTelemetryCollector();
     this.databaseCollector = new DatabaseTelemetryCollector();
   }
@@ -21,12 +20,11 @@ export class MainTelemetryCollector implements TelemetryCollector {
     const dbData = await this.databaseCollector.getTelemetry(filter);
 
     const merged = [...cachedData, ...dbData];
-    merged.sort((a,b)=>{
+    merged.sort((a, b) => {
       return a.createdAt.getTime() - b.createdAt.getTime();
     });
 
     return merged;
-
   }
 
   saveTelemetry(telemetry: ITelemetry) {
