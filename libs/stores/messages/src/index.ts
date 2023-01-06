9fe40acca8d4f98479afc3cd20807a2d8e2c7725
Martin Fitzek <m4rfitzek@gmail.com>
2023-01-06 09:24:45 +0100

    message composition

--- libs/stores/messages/src/index.ts
+++ libs/stores/messages/src/index.ts
@@ -1,14 +1,24 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
 export type Message = {
   message: string;
   type: 'info' | 'success' | 'warning' | 'error';
 };
 
-export const useMessagesStore = defineStore('messages', {
-  state: () => {
-    return {
-      messages: [] as Message[],
-    };
-  },
+export const useMessagesStore = defineStore('messages', () => {
+  const messages = ref<Message[]>([]);
+
+  function notify(message: Message) {
+    messages.value.push(message);
+  }
+
+  function getMessage() {
+    if (messages.value.length === 0) {
+      return null;
+    }
+    return messages.value.shift();
+  }
+
+  return { messages, getMessage, notify };
 });
