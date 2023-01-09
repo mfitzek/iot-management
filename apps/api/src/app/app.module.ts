a8c98b4cf824e8387840c33b9aef13d0b3741d08
Martin Fitzek <m4rfitzek@gmail.com>
2023-01-09 09:07:03 +0100

    Gateway api module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { GatewayModule } from './../gateway/gateway.module';
 import { AdministritonModule } from './../administration/administration.module';
 import { AuthModule } from './../auth/auth.module';
 import { DeviceModule } from './../device/device.module';
@@ -9,7 +10,14 @@ import { TelemetryModule } from '../telemetry/telemetry.module';
 import { SettingsModule } from '../settings/settings.module';
 
 @Module({
-  imports: [DeviceModule, AuthModule, TelemetryModule, SettingsModule, AdministritonModule],
+  imports: [
+    DeviceModule,
+    AuthModule,
+    TelemetryModule,
+    SettingsModule,
+    AdministritonModule,
+    GatewayModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
