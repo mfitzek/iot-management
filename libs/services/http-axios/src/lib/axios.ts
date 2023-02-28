b10004ebceed674237677b5786cd5b3cd70a2bd1
Martin Fitzek <m4rfitzek@gmail.com>
2023-02-28 22:09:00 +0100

    wip

--- libs/services/http-axios/src/lib/axios.ts
+++ libs/services/http-axios/src/lib/axios.ts
@@ -4,12 +4,15 @@ import Axios, { AxiosInstance } from 'axios';
 // TODO: Move to production/development configuration file
 const API_URL = import.meta.env['VITE_API_SERVER'];
 
+console.log('URL', API_URL, import.meta.env);
+
 export default class AxiosClient {
   private static _axios: AxiosInstance = Axios.create({
     baseURL: API_URL,
   });
 
   public static get instance() {
+    console.log('instance', this._axios);
     return this._axios;
   }
 }
